a0e968af57ddbcd5f90bc958d72ee8733f79abce
daliakhammash <dalia.khammash@outlook.com>
2018-10-20 20:26:32 +0300

    first solution of stringifyJSON function we need to refactor it to handle the array case and another small details

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,22 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+  var arr = Object.keys(obj);
+  var value = obj[arr[0]];
+  delete obj[arr[0]];
+  if (!arr.length) {
+  	return '}';
+  } else if (typeof value === 'string') {
+  	 return '"' + arr[0] + '": "' + value + '", ' + stringifyJSON(obj) 
+  } 
+  else if (typeof value === 'object') {
+  	 if(Array.isArray(value)){
+  	   return '"' + arr[0] + '": [' + value + '], ' + stringifyJSON(obj)
+  	 }
+  	 return stringifyJSON(value) ;
+  }
+  return '"' + arr[0] + '": ' + value + ', ' + stringifyJSON(obj);
+ };
+
+
+// var 
